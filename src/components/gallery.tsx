d34d5a4d2bd135328b1345628fc979e4fafd9c41
Jeff Mastry <jeff@mastry.net>
2019-05-12 09:53:20 -0400

    Remove unused imports

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Row, Col, Card, CardBody, CardTitle, Container } from "reactstrap";
 import ComingSoon from "./ComingSoon";
 
 export class Gallery extends React.Component<any, any> {
--- src/components/simulator.tsx
+++ src/components/simulator.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Row, Col, Card, CardBody, CardTitle, Container } from "reactstrap";
 import ComingSoon from "./ComingSoon";
 
 export class Simulator extends React.Component<any, any> {
