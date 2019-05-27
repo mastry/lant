ce136d43358aebf4e40883df36ca20dcff00f8bf
Jeff Mastry <jeff@mastry.net>
2019-05-26 22:39:56 -0400

    Add title property to AntBar

--- src/components/shared/antBar.tsx
+++ src/components/shared/antBar.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Row, Col, Navbar, NavbarBrand } from "reactstrap";
 import { Logo } from "../shared/logo";
+import { interfaceDeclaration } from '@babel/types';
 
-const AntBar: React.FC = () => {
+export interface IProps {
+    title: string
+}
+
+const AntBar: React.FC<IProps> = (props: IProps) => {
     return (
         <Row>
             <Col>
                 <Navbar dark expand="lg" color="dark" >
-                    <NavbarBrand href="/" className="mr-4">
-                        <Logo width={48} height={48} light />Langton's Ant Simulator
-                </NavbarBrand>
+                    <NavbarBrand href="/">
+                        <Logo width={48} height={48} light />{props.title}
+                    </NavbarBrand>
                 </Navbar>
             </Col>
         </Row>
